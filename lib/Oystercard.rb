0fe031e8fbb2a2523a310894ebe82e5ce4365b0b
toby <toby.hw@live.co.uk>
2017-05-15 16:24:04 +0100

    Adds tests for #top_up, updates #top_up method to pass

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -2,8 +2,12 @@ class Oystercard
 
   attr_reader :balance
 
-  def initialize
+  def initialize(balance = 0)
     @balance = balance
+  end
+
+  def top_up(amount)
+    @balance += amount
   end
 
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -7,7 +7,19 @@ describe Oystercard do
   end
 
   it "contains @balance attribute" do
-      expect(subject.balance).to eq @balance
+      expect(subject.balance).to eq 0
+  end
+
+  describe '#top_up' do
+
+    it "allows user to top up balance" do
+      expect(subject).to respond_to :top_up
+    end
+
+    it "increases balance by amount given" do
+      expect(subject.top_up(5)).to eq 5
+    end
+
   end
 
 end
