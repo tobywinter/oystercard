9884b69fef38dbbec7cd3213c50bd8135a3fb2f1
Kavita <kkavita92@gmail.com>
2017-05-16 16:21:23 +0100

    Refactored existing code - Used doubles for entry and exit stations - Topped up card before running all tests (except ones checking for min balance) - Tests to use named subjects of card and empty_card

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,40 +1,45 @@
 require 'Oystercard'
 
 describe Oystercard do
+  let (:entry_station) {double(:station)}
+  let (:exit_station)  {double(:station)}
+  subject(:card) { described_class.new }
+  subject(:empty_card) { described_class.new }
+  before {card.top_up(20)}
 
-  it "responds to #balance" do
-    expect(subject).to respond_to :balance
-  end
+  describe 'responsiveness' do
+    it "responds to #balance" do
+      expect(card).to respond_to :balance
+    end
 
-  it "contains @balance attribute" do
-      expect(subject.balance).to eq 0
-  end
+    it "contains @balance attribute" do
+        expect(card.balance).to eq 20
+    end
 
-  it 'responds to entry_station' do
-    expect(subject).to respond_to :entry_station
-  end
+    it 'responds to entry_station' do
+      expect(card).to respond_to :entry_station
+    end
 
-  it 'responds to journeys' do
-    expect(subject).to respond_to :journeys
+    it 'responds to journeys' do
+      expect(card).to respond_to :journeys
+    end
   end
 
   context 'recording journey' do
-    let (:entry_station) {double(:station)}
-    let (:exit_station)  {double(:station)}
-    before {subject.top_up(20)}
+
 
     it 'has an empty list of journeys by default' do
-      expect(subject.journeys).to be_empty
+      expect(card.journeys).to be_empty
     end
 
     it 'checks that touching in saves entry station' do
-      expect {subject.touch_in(entry_station)}.to change{subject.journeys}.to eq([{entry_station: entry_station}])
+      expect {card.touch_in(entry_station)}.to change{card.journeys}.to eq([{entry_station: entry_station}])
     end
 
     it 'checks that touching out saves exit station' do
-      subject.touch_in(entry_station)
-      subject.touch_out(exit_station)
-      expect(subject.journeys).to eq([{entry_station: entry_station, exit_station: exit_station}])
+      card.touch_in(entry_station)
+      card.touch_out(exit_station)
+      expect(card.journeys).to eq([{entry_station: entry_station, exit_station: exit_station}])
     end
 
   end
@@ -42,67 +47,59 @@ describe Oystercard do
   describe '#top_up' do
 
     it "allows user to top up balance" do
-      expect(subject).to respond_to :top_up
+      expect(card).to respond_to :top_up
     end
 
     it "increases balance by amount given" do
-      expect(subject.top_up(5)).to eq 5
+      expect(card.top_up(5)).to eq 25
     end
 
     it "raises error 'Exceeds maximum limit' message" do
-      expect {subject.top_up(91)}.to raise_error("Exceeds maximum limit: Balance must not exceed £#{Oystercard::MAX_LIMIT}")
+      expect {card.top_up(71)}.to raise_error("Exceeds maximum limit: Balance must not exceed £#{Oystercard::MAX_LIMIT}")
     end
 
   end
 
     it "@max_limit is set to 90 pounds" do
-      expect(subject.max_limit).to eq 90
+      expect(card.max_limit).to eq 90
     end
 
   describe "#in_journey?" do
     it "Checks if the card is not in journey" do
-      expect(subject).not_to be_in_journey
+      expect(card).not_to be_in_journey
     end
   end
 
   describe "#below_minimum_balance" do
 
     it "returns true when below @minimum_balace" do
-      expect(subject.below_minimum_balance).to eq true
+      expect(empty_card.below_minimum_balance).to eq true
     end
 
     it "returns false when above @minimum_balace" do
-      oystercard = Oystercard.new(30)
-      expect(oystercard.below_minimum_balance).to eq false
+      expect(card.below_minimum_balance).to eq false
     end
 
   end
 
 
   describe "#touch_in" do
-    let(:station) {double(:station)}
 
     it "will raise error 'Insufficient funds' if below minimum balance" do
-      expect {subject.touch_in(station)}.to raise_error("Insufficient funds: Minimum balance - £#{Oystercard::MINIMUM_BALANCE}")
+      expect {empty_card.touch_in(entry_station)}.to raise_error("Insufficient funds: Minimum balance - £#{Oystercard::MINIMUM_BALANCE}")
     end
 
     it "will save station in @entry_station" do
-      subject.top_up(20)
-      expect {subject.touch_in(station)}.to change{subject.entry_station}.to eq(station)
+      expect {card.touch_in(entry_station)}.to change{card.entry_station}.to eq(entry_station)
     end
 
   end
 
   describe "#touch_out" do
-    let(:fare) {fare = 1}
-    let(:exit_station) {double(:station)}
-    let(:entry_station) {double(:station)}
-    before {subject.top_up(20)}
-
 
     it 'deducts fare from balance upon touch out' do
-      subject.touch_in(entry_station)
-      expect {subject.touch_out(fare,exit_station)}.to change{subject.balance}.by(-fare)
+      card.touch_in(entry_station)
+      expect {card.touch_out(exit_station)}.to change{card.balance}.by(-Oystercard::MIN_FARE)
     end
 
   end
